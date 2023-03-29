fb0d0db3efb2f849065231ee1e503f0fe7bdac1d
jahidul39306 <72722599+jahidul39306@users.noreply.github.com>
2023-03-30 01:29:25 +0600

    story complete

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
+import Feed from "@/components/Feed";
 import Header from "@/components/Header";
 import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram Clone</title>
       </Head>
-      
+
       {/* Header */}
       <Header />
       {/* Feed */}
-
+      <Feed />
       {/* Modal */}
     </div>
   );
