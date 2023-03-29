5d3477216f4c94761ea568fef302a5a4550c9ad8
jahidul39306 <72722599+jahidul39306@users.noreply.github.com>
2023-03-30 00:05:57 +0600

    Header complete

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 
 function Header() {
   return (
-    <div>
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* left */}
         <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
@@ -47,16 +47,19 @@ function Header() {
         <div className="flex items-center justify-end space-x-4">
           <Bars3Icon className="h-6 md:hidden cursor-pointer" />
           <HomeIcon className="navBtn" />
-          <PaperAirplaneIcon className="navBtn"/> 
-          <PlusCircleIcon className="navBtn"/>
-          <UserGroupIcon className="navBtn"/>
-          <HeartIcon className="navBtn"/>
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn -rotate-45" />
+            <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+          </div>
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
 
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3zo4EAEtqcvbiwxdlgnNUUgMrqbIPezOkDKeoFLRLdoCamQ3t23r8C5hePqGbqgROu_k&usqp=CAU"
             alt="profile pic"
             className="h-10 rounded-full cursor-pointer"
-          /> 
+          />
         </div>
       </div>
     </div>
