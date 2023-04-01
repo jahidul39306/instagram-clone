f8e4b3d0de97f485f7b9b8452a54bf46c1a45b2a
jahidul39306 <72722599+jahidul39306@users.noreply.github.com>
2023-04-01 22:13:10 +0600

    Dynamic user data showing

--- components/Header.js
+++ components/Header.js
@@ -8,20 +8,31 @@ import {
   Bars3Icon,
 } from "@heroicons/react/24/outline";
 import { HomeIcon } from "@heroicons/react/24/solid";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function Header() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* left */}
-        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
+        <div
+          onClick={() => router.push("/")}
+          className="relative hidden lg:inline-grid w-24 cursor-pointer"
+        >
           <Image
             src="https://links.papareact.com/ocw"
             fill
             style={{ objectFit: "contain" }}
           />
         </div>
-        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div
+          onClick={() => router.push("/")}
+          className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer"
+        >
           <Image
             src="https://img.icons8.com/ios/50/null/instagram-new--v1.png"
             fill
@@ -46,20 +57,30 @@ function Header() {
         {/* right */}
         <div className="flex items-center justify-end space-x-4">
           <Bars3Icon className="h-6 md:hidden cursor-pointer" />
-          <HomeIcon className="navBtn" />
-          <div className="relative navBtn">
-            <PaperAirplaneIcon className="navBtn -rotate-45" />
-            <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
-          </div>
-          <PlusCircleIcon className="navBtn" />
-          <UserGroupIcon className="navBtn" />
-          <HeartIcon className="navBtn" />
+          <HomeIcon onClick={() => router.push("/")} className="navBtn" />
+          {session ? (
+            <>
+              <div className="relative navBtn">
+                <PaperAirplaneIcon className="navBtn -rotate-45" />
+                <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+                  3
+                </div>
+              </div>
+              <PlusCircleIcon className="navBtn" />
+              <UserGroupIcon className="navBtn" />
+              <HeartIcon className="navBtn" />
 
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3zo4EAEtqcvbiwxdlgnNUUgMrqbIPezOkDKeoFLRLdoCamQ3t23r8C5hePqGbqgROu_k&usqp=CAU"
-            alt="profile pic"
-            className="h-10 rounded-full cursor-pointer"
-          />
+              <img
+                // src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3zo4EAEtqcvbiwxdlgnNUUgMrqbIPezOkDKeoFLRLdoCamQ3t23r8C5hePqGbqgROu_k&usqp=CAU"
+                onClick={signOut}
+                src={session?.user?.image}
+                alt="profile pic"
+                className="h-10 w-10 rounded-full cursor-pointer"
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign In</button>
+          )}
         </div>
       </div>
     </div>
--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,4 +1,8 @@
+import { signOut, useSession } from "next-auth/react";
+
 function MiniProfile() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div
       className="flex items-center justify-between 
@@ -6,14 +10,17 @@ function MiniProfile() {
     >
       <img
         className="w-16 h-16 rounded-full border p-[2px]"
-        src="https://images.unsplash.com/photo-1679419930974-e8171969aea0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        // src="https://images.unsplash.com/photo-1679419930974-e8171969aea0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        src={session?.user?.image}
         alt=""
       />
       <div className="flex-1 mx-4">
-        <h2 className="font-bold">random</h2>
+        <h2 className="font-bold">{session?.user?.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to instagram</h3>
       </div>
-      <button className="text-blue-400 text-sm font-semibold">Sign Out</button>
+      <button onClick={signOut} className="text-blue-400 text-sm font-semibold">
+        Sign Out
+      </button>
     </div>
   );
 }
